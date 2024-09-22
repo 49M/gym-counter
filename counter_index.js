fb503c65bd30b592e34082b3033b68baa9d7dc8e
49M <74563947+49M@users.noreply.github.com>
2024-09-22 11:36:35 -0400

    Added buttons for set and made fixes

--- counter_index.js
+++ counter_index.js
@@ -1,25 +1,30 @@
-let pushExercises = 0
-let pullExercises = 0
-let legExercises = 0
-let exerciseCount = pushExercises + pullExercises + legExercises
-let first = "Michal"
-let last = "Buczek"
-console.log(first + ' ' + last)
+let exerciseCount = 0
+let setCount = 0
 let previousEntries = document.getElementById("save-el")
 let countExercise = document.getElementById("count_exercise")
+let countSet = document.getElementById("count_set")
+let previousSets = document.getElementById("save-sets")
 
 function increment() {
-
     exerciseCount += 1
     countExercise.innerText = exerciseCount
 }
 
-
 function save() {
-    exerciseCount
     previousEntries.textContent += exerciseCount + " - "
     exerciseCount = 0
     countExercise.textContent = exerciseCount
+}
+
+function increment_set() {
+    setCount += 1
+    countSet.textContent = setCount
+}
+
+function save_set() {
+    previousSets.textContent += countSet.textContent + " - "
+    setCount = 0
+    countSet.textContent = setCount
 }
 
 
