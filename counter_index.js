edb80f3bffe080772b5c94c54b0c782829721ba4
Michal Buczek <74563947+49M@users.noreply.github.com>
2024-09-22 10:53:56 -0400

    Uploaded files

---
+++ counter_index.js
@@ -0,0 +1,29 @@
+// document.getElementById("count_exercise").innerText = 2
+// document.getElementById("count_set").innerText = 3
+
+let pushExercises = 0
+let pullExercises = 0
+let legExercises = 0
+let exerciseCount = pushExercises + pullExercises + legExercises
+let first = "Michal"
+let last = "Buczek"
+console.log(first + ' ' + last)
+let previousEntries = document.getElementById("save-el")
+let countExercise = document.getElementById("count_exercise")
+
+function increment() {
+
+    exerciseCount += 1
+    countExercise.innerText = exerciseCount
+}
+
+
+function save() {
+    exerciseCount
+    previousEntries.textContent += exerciseCount + " - "
+    exerciseCount = 0
+    countExercise.textContent = exerciseCount
+}
+
+
+
